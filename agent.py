b109730109c90b693c1002836b87af0e274d5679
cyrine anene <cyrine.anene@supcom.tn>
2025-02-28 15:43:13 +0100

    changes

--- agent.py
+++ agent.py
@@ -1,4 +1,28 @@
-from creawai import Agent, Task, Crew
-from crew_ai import SerperDevtool
-from langchain_ibm import WatsonxLLM
+from crewai import Agent, Task, Crew
+from crewai_tools import SerperDevTool
+from langchain_openai import ChatOpenAI
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+#Step 1: Initiate the API Keys
+
+GROQ_APIKEY = os.getenv('GROQ_APIKEY')
+SERPER_APIKEY = os.getenv("SERPER_APIKEY")
+
+from groq import Groq
+
+client = Groq(api_key=GROQ_APIKEY)
+
+completion = client.chat.completions.create(
+    model="deepseek-r1-distill-llama-70b",
+    messages=[],
+    temperature=0.6,
+    max_completion_tokens=4096,
+    top_p=0.95,
+    stream=True,
+    stop=None,
+)
+
+for chunk in completion:
+    print(chunk.choices[0].delta.content or "", end="")
